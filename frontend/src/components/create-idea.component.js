3c9a007e4204175179031005a7dfc207f9e127f1
Theveenan <theveenan@gmail.com>
2022-01-22 17:26:17 -0500

    Just at POST error

--- frontend/src/components/create-idea.component.js
+++ frontend/src/components/create-idea.component.js
@@ -4,19 +4,85 @@ export default class CreateIdea extends Component {
     constructor(props){
         super(props);
 
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
         this.state = {
             username: '',
             description: '',
+            users: []
+        }
+    }
+
+    componentDidMount(){
+        this.setState({
+            users: ['test user'],
+            username: 'test user'
+        })
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        })
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        const idea = {
+            username: this.state.username,
+            desription: this.state.description,
         }
 
-        
-
+        console.log(idea);
+        window.location = '/';
     }
 
     render(){
         return(
             <div>
-                <p>You are on the Create Idea Component!</p>
+                <h3>Create New Idea!</h3>
+                <form onSubmit = {this.onSubmit}>
+
+                    <div className = "form-group">
+                        <label>Username: </label>
+                        <select ref="userInput"
+                        required
+                        className="form-control"
+                        value={this.state.username}
+                        onChange={this.onChangeUsername}>
+                            {
+                                this.state.users.map(function(user) {
+                                return <option 
+                                    key={user}
+                                    value={user}>{user}
+                                    </option>;
+                                })
+                            }
+                        </select>
+                    </div>
+                    <div className="form-group"> 
+                        <label>Description: </label>
+                        <input  type="text"
+                            required
+                            className="form-control"
+                            value={this.state.description}
+                            onChange={this.onChangeDescription}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+                    </div>
+
+                </form>
+
             </div>
         )
     }
